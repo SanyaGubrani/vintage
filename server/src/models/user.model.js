6bdebb31a7035107ac6fbf69d1e52044ec21f3d0
SanyaGubrani <gubrani.sanya2@gmail.com>
2025-03-31 18:51:20 +0530

    add password hashing

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -44,4 +44,18 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to check password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 export const User = mongoose.model("User", userSchema);
