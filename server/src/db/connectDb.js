8abcebd27a2bdee031ed72a878a77e6179a77935
SanyaGubrani <gubrani.sanya2@gmail.com>
2025-04-02 14:23:43 +0530

    add name for mongodb uri

--- server/src/db/connectDb.js
+++ server/src/db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/vintage`);
     console.log(`MongoDb connection successful: ${conn.connection.host}`);
   } catch (error) {
     console.log("Error connecting to Mongodb: ", error);
@@ -11,4 +11,3 @@ const connectDb = async () => {
 };
 
 export default connectDb;
-
