b1e6a81575b510574a38bdd79998d125c6f2bfec
SanyaGubrani <gubrani.sanya2@gmail.com>
2025-05-13 22:57:50 +0530

    refactor like model

--- server/src/models/like.model.js
+++ server/src/models/like.model.js
@@ -5,35 +5,18 @@ const likeSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
 
     post: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Post",
-    },
-
-    comment: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
+      required: true,
     },
   },
   { timestamps: true }
 );
 
-// Enforce one target only (Like on comment OR post)
-likeSchema.pre("save", function (next) {
-  if ((this.post && this.comment) || (!this.post && !this.comment)) {
-    return next(
-      new Error(
-        "A like must reference either a post OR a comment, not both or neither"
-      )
-    );
-  }
-  next();
-});
-
-// Prevent duplicate likes
-likeSchema.index({ user: 1, post: 1 }, { unique: true, sparse: true });
-likeSchema.index({ user: 1, comment: 1 }, { unique: true, sparse: true });
+likeSchema.index({ user: 1, post: 1 }, { unique: true });
 
 export const Like = mongoose.model("Like", likeSchema);
